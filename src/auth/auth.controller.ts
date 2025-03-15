994208c0815e04bdb61cb345a8872a1ed5a6de63
JuannDarley <juandarley99@gmail.com>
2025-03-15 13:38:57 -0300

    implement token e validate on route me

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common'
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common'
 import { SigninDto, SignupDto } from './dtos/auth'
 import { AuthService } from './auth.service'
+import { AuthGuard } from './auth.guard'
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +18,11 @@ export class AuthController {
 	@Post('signin')
 	async signin(@Body() body: SigninDto) {
 		return this.authService.signin(body)
+	}
+
+	@UseGuards(AuthGuard)
+	@Get('me')
+	async me(@Request() request) {
+		return request.user
 	}
 }
